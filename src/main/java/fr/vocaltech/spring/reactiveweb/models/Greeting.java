297153efceaa6554e4f737cb4c2b716a3abc1b75
vocaltech <it@vocaltech.fr>
2022-03-13 15:08:33 +0100

    Implementation with lombok

--- src/main/java/fr/vocaltech/spring/reactiveweb/models/Greeting.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/models/Greeting.java
@@ -1,4 +1,12 @@
 package fr.vocaltech.spring.reactiveweb.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Greeting {
+    private String message;
 }
