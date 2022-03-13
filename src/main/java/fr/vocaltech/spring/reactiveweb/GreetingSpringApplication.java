7c54844d73ea8baf5477e36f8542a1a991f13121
vocaltech <it@vocaltech.fr>
2022-03-13 15:09:38 +0100

    Implementation

--- src/main/java/fr/vocaltech/spring/reactiveweb/GreetingSpringApplication.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/GreetingSpringApplication.java
@@ -1,4 +1,17 @@
 package fr.vocaltech.spring.reactiveweb;
 
+import fr.vocaltech.spring.reactiveweb.clients.GreetingClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class GreetingSpringApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(GreetingSpringApplication.class);
+        GreetingClient greetingClient = ctx.getBean(GreetingClient.class);
+
+        // We need to block for the content here or the JVM might exit before the message is logged
+        System.out.println(greetingClient.getMessage().block());
+    }
 }
--- src/main/java/fr/vocaltech/spring/reactiveweb/handlers/GreetingHandler.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/handlers/GreetingHandler.java
@@ -1,4 +1,21 @@
 package fr.vocaltech.spring.reactiveweb.handlers;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import fr.vocaltech.spring.reactiveweb.models.Greeting;
+
+@Component
 public class GreetingHandler {
+    public Mono<ServerResponse> hello(ServerRequest req) {
+        return ServerResponse
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(new Greeting("Hello Spring Reactive from GreetingHandler !")));
+    }
+
 }
