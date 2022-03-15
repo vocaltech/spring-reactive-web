a79447424c65fd22d866495975e81617f37e364f
vocaltech <it@vocaltech.fr>
2022-03-15 22:40:01 +0100

    Update with EmployeeClient

--- src/main/java/fr/vocaltech/spring/reactiveweb/EmployeeSpringApplication.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/EmployeeSpringApplication.java
@@ -1,12 +1,17 @@
 package fr.vocaltech.spring.reactiveweb;
 
+import fr.vocaltech.spring.reactiveweb.clients.EmployeeClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class EmployeeSpringApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeSpringApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(EmployeeSpringApplication.class, args);
+
+		EmployeeClient client = ctx.getBean(EmployeeClient.class);
+		client.consume();
 	}
 
 }
