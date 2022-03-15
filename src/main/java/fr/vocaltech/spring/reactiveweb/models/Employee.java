685aa91386e276c9be162890d7577aaa5cb3e9c7
vocaltech <it@vocaltech.fr>
2022-03-15 22:35:23 +0100

    implement wih lombok

--- src/main/java/fr/vocaltech/spring/reactiveweb/models/Employee.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/models/Employee.java
@@ -1,4 +1,13 @@
 package fr.vocaltech.spring.reactiveweb.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+    private long id;
+    private String name;
 }
