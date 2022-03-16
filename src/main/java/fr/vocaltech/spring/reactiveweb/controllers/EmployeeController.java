0e902a4654589dc0fde5b30621bdf7c040b65f18
vocaltech <it@vocaltech.fr>
2022-03-16 23:36:06 +0100

    Refactor with Autowired annotation

--- src/main/java/fr/vocaltech/spring/reactiveweb/controllers/EmployeeController.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/controllers/EmployeeController.java
@@ -1,20 +1,19 @@
 package fr.vocaltech.spring.reactiveweb.controllers;
 
-import fr.vocaltech.spring.reactiveweb.models.Employee;
-import fr.vocaltech.spring.reactiveweb.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import fr.vocaltech.spring.reactiveweb.models.Employee;
+import fr.vocaltech.spring.reactiveweb.repositories.EmployeeRepositoryMap;
+
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    private final EmployeeRepository employeeRepository;
-
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
+    @Autowired
+    private EmployeeRepositoryMap employeeRepository;
 
     @GetMapping
     private Flux<Employee> getAllEmployees() {
