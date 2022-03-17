ae5001bfe418f0185fd231d33d2a52d5cb6340e3
vocaltech <it@vocaltech.fr>
2022-03-17 23:55:02 +0100

    Extends MongoRepository

--- src/main/java/fr/vocaltech/spring/reactiveweb/repositories/AccountCrudRepository.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/repositories/AccountCrudRepository.java
@@ -1,12 +1,21 @@
 package fr.vocaltech.spring.reactiveweb.repositories;
 
 import fr.vocaltech.spring.reactiveweb.models.Account;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
-@Repository
+import java.util.List;
+
+/*
 public interface AccountCrudRepository extends ReactiveCrudRepository<Account, String> {
     Flux<Account> findAll();
+}
+*/
+
+public interface AccountCrudRepository extends MongoRepository<Account, String> {
+    @Override
+    List<Account> findAll();
 }
