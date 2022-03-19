db78ba40526db3f4d3f2af66216bdfe6712690fc
vocaltech <it@vocaltech.fr>
2022-03-19 12:23:45 +0100

    adds count() method

--- src/main/java/fr/vocaltech/spring/reactiveweb/repositories/AccountCrudRepository.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/repositories/AccountCrudRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
 import fr.vocaltech.spring.reactiveweb.models.Account;
+import reactor.core.publisher.Mono;
 
 public interface AccountCrudRepository extends ReactiveCrudRepository<Account, String> {
     Flux<Account> findAll();
+    Mono<Long> count();
 }
