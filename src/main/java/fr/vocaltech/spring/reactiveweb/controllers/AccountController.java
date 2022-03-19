5b4a78c18c65e5092737d70484ccd774225404fa
vocaltech <it@vocaltech.fr>
2022-03-19 15:29:37 +0100

    Initial commit

---
+++ src/main/java/fr/vocaltech/spring/reactiveweb/controllers/AccountController.java
@@ -0,0 +1,32 @@
+package fr.vocaltech.spring.reactiveweb.controllers;
+
+import fr.vocaltech.spring.reactiveweb.models.Account;
+import fr.vocaltech.spring.reactiveweb.repositories.AccountCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+    @Autowired
+    AccountCrudRepository accountCrudRepository;
+
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Account> getAccounts() {
+        return accountCrudRepository.findAll();
+    }
+
+    @GetMapping(
+            path = "/{id}",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE
+    )
+    public Mono<Account> getAccountById(@PathVariable String id) {
+        return accountCrudRepository.findById(id);
+    }
+}
