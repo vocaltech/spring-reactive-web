dfdd4527860ffc73530ea9c08f847fbe7032f487
vocaltech <it@vocaltech.fr>
2022-03-20 16:48:27 +0100

    Add Document annotation

--- src/main/java/fr/vocaltech/spring/reactiveweb/models/Account.java
+++ src/main/java/fr/vocaltech/spring/reactiveweb/models/Account.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "accounts")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Account {
     @Id
     private String id;
+
     private String owner;
     private Double balance;
 }
